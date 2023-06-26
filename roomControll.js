b412493bd6e8e9b664fc1906c6e1043dfd0c5e81
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-06-26 21:04:59 +0600

    Add files via upload

--- roomControll.js
+++ roomControll.js
@@ -110,23 +110,24 @@ function roomShow(day, hour){
         //start from sunday
     
             //sunday 0
+            //room number 5001 5002 701 
             [[
-                [0,0,0,1,1,0,1,1,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
+                [0,1,1,1,1,0,1,1,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
+                [1,1,0,1,1,0,0,1,1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0,0] //floor 4
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
+                [0,1,1,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0,0] //floor 2
             ],[
                 [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 1
             ]],
 
             //Monday 1
             [[
-                [1,1,0,1,1,0,1,1,1],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
+                [1,1,0,1,1,0,1,1,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
+                [0,0,1,1,0,1,1,1,1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,0,1,1,1],[0,0,0,0,0,0,0,0,0] //floor 4
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
+                [1,1,0,0,0,0,1,1,0],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,0,1,1,1],[0,0,0,0,0,0,0,0,0] //floor 2
             ],[
                 [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 1
             ]],
@@ -134,19 +135,19 @@ function roomShow(day, hour){
             [[
                 [1,1,1,1,0,1,1,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
+                [1,1,1,1,0,0,0,1,1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
+                [1,1,0,1,1,0,0,1,1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
             ],[
                 [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 1
             ]],
             //Wednesday 3
             [[
-                [1,1,0,1,1,0,1,1,1],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
+                [1,1,0,1,1,0,1,1,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
+                [1,1,0,0,0,1,1,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
+                [1,1,1,1,0,1,1,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
             ],[
                 [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 1
             ]],
@@ -154,9 +155,9 @@ function roomShow(day, hour){
             [[
                 [0,0,1,1,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 5
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
+                [1,1,0,0,1,1,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 4
             ],[
-                [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
+                [0,0,1,1,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 2
             ],[
                 [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0] //floor 1
             ]],
@@ -194,8 +195,131 @@ function roomShow(day, hour){
         R5001.style.backgroundColor = "red";
     }
 
+    if(roomInfo[day][0][1][hour]== 0){
+        R5002.style.backgroundColor = "green";
+    }else{
+        R5002.style.backgroundColor = "red";
+    }
+
+    if(roomInfo[day][0][2][hour]== 0){
+        L5003.style.backgroundColor = "green";
+    }else{
+        L5003.style.backgroundColor = "red";
+    }
+
+
+//floor 4 index 1  
+   
+
+    //[day][floor][room][hour]
+    if(roomInfo[day][1][0][hour]== 0){
+        R4001.style.backgroundColor = "green";
+    }else{
+        R4001.style.backgroundColor = "red";
+    }
+
+    if(roomInfo[day][1][1][hour]== 0){
+        R4002.style.backgroundColor = "green";
+    }else{
+        R4002.style.backgroundColor = "red";
+    }
+
+    if(roomInfo[day][1][2][hour]== 0){
+        L4701.style.backgroundColor = "green";
+    }else{
+        L4701.style.backgroundColor = "red";
+    }
+
+
+
+//floor 2 index 2  
+   
+
+    //[day][floor][room][hour]
+    if(roomInfo[day][2][0][hour]== 0){
+        R2001.style.backgroundColor = "green";
+    }else{
+        R2001.style.backgroundColor = "red";
+    }
+
+    if(roomInfo[day][2][1][hour]== 0){
+        R2002.style.backgroundColor = "green";
+    }else{
+        R2002.style.backgroundColor = "red";
+    }
+
+    if(roomInfo[day][2][2][hour]== 0){
+        L2701.style.backgroundColor = "green";
+    }else{
+        L2701.style.backgroundColor = "red";
+    }
+
+
+
+    
+    selectShowButton(day, hour);
     
 
  }
 
  roomShow(day, hour);
+
+ selectShowButton(day, hour);
+
+//select but it will be red if next another button it will be green
+ function selectShowButton(day, hour){
+
+
+    Saturday.style.backgroundColor = "green";
+    Sunday.style.backgroundColor = "green"; 
+    Monday.style.backgroundColor = "green"; 
+    Tuesday.style.backgroundColor = "green";
+    Wednesday.style.backgroundColor = "green";
+    Thursday.style.backgroundColor = "green";
+    Friday.style.backgroundColor = "green";
+        if(day==6)
+        Saturday.style.backgroundColor = "red"; 
+        if(day==0)
+        Sunday.style.backgroundColor = "red"; 
+        if(day==1)
+        Monday.style.backgroundColor = "red"; 
+        if(day==2)
+        Tuesday.style.backgroundColor = "red"; 
+        if(day==3)
+        Wednesday.style.backgroundColor = "red"; 
+        if(day==4)
+        Thursday.style.backgroundColor = "red"; 
+       if(day==5)
+        Friday.style.backgroundColor = "red"; 
+     //for time button
+     btn8.style.backgroundColor = "green";
+     btn9.style.backgroundColor = "green";
+     btn10.style.backgroundColor = "green";
+     btn11.style.backgroundColor = "green";
+     btn12.style.backgroundColor = "green";
+     btn1.style.backgroundColor = "green";
+     btn2.style.backgroundColor = "green";
+     btn3.style.backgroundColor = "green";
+     btn4.style.backgroundColor = "green";
+
+     if(hour == 0)
+     btn8.style.backgroundColor = "red";
+     if(hour == 1)
+     btn9.style.backgroundColor = "red";
+     if(hour == 2)
+     btn10.style.backgroundColor = "red";
+     if(hour == 3)
+     btn11.style.backgroundColor = "red";
+     if(hour == 4)
+     btn12.style.backgroundColor = "red";
+     if(hour == 5)
+     btn1.style.backgroundColor = "red";
+     if(hour == 6)
+     btn2.style.backgroundColor = "red";
+     if(hour == 7)
+     btn3.style.backgroundColor = "red";
+     if(hour == 8)
+     btn4.style.backgroundColor = "red";
+     
+
+ }
